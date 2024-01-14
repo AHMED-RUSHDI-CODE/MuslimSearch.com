95ddf1fa10dd3fbb5ee422a7b3812097cd2b4ead
AHMED-RUSHDI-CODE <156532814+AHMED-RUSHDI-CODE@users.noreply.github.com>
2024-01-14 23:32:05 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Search Engine</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div class="search-container">
-        <img src="google-logo.png" alt="Islam Search Engine" class="logo">
-        <div class="search-box">
-            <input type="text" id="search-query" placeholder="Search...">
-            <button onclick="search()">Search</button>
-        </div>
-        <div class="buttons">
-            <button onclick="feelingLucky()">I'm Feeling Lucky</button>
-        </div>
-    </div>
+import '@styles/globals.css'
 
-    <script>
-        // تعريف وظيفة البحث
-        function search() {
-            // قم بتنفيذ البحث هنا
-            console.log('Performing search...');
-        }
+function Application({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
 
-        // تعريف وظيفة "I'm Feeling Lucky"
-        function feelingLucky() {
-            // قم بتنفيذ الإجراء الذي تريده عند الضغط على "I'm Feeling Lucky"
-            console.log('Feeling Lucky!');
-        }
-    </script>
-</body>
-</html>
+export default Application
